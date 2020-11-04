4f3a8a3612b5ea0714e589c7f3497554cd36ff2c
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-04 17:06:52 +0100

    slide down fixed

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@
 $('nav ul li').each(function(){
   $(this).click(function(){
     $('nav ul li').each(function(){
-      $(this).children('.dropdown').slideUp()
+      $(this).children('.dropdown').slideUp();
     })
-  $(this).children('.dropdown').slideDown()
+    if($(this).children('.dropdown').is(':visible')){
+      $(this).children('.dropdown').slideUp();
+    }else{
+      $(this).children('.dropdown').slideDown()
+    }
   })
 })
