d8ca92959c06a6f25ffcc969500810201c37de78
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-04 16:59:25 +0100

    slide added

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@
 $('nav ul li').each(function(){
   $(this).click(function(){
     $('nav ul li').each(function(){
-      $(this).children('.dropdown').hide()
+      $(this).children('.dropdown').slideUp()
     })
-  $(this).children('.dropdown').toggle()
+  $(this).children('.dropdown').slideDown()
   })
 })
