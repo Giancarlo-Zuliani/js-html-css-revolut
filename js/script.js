eff97d450d0ee76f3bff0a961edb1f99409d7c2b
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-05 09:42:56 +0100

    last commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
-
-$('nav ul > li').each(function(){
-  $(this).click(function(){
+  $('nav ul>li').click(function(){
     var el = $(this).children('.dropdown');
     $('nav ul li').each(function(){
       $(this).children('.dropdown').slideUp();
     })
     el.is(':visible') ? el.slideUp() : el.slideDown();
-  })
 });
 
 
