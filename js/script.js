b512cbf196a42813c8daff2048bd0346c82dc174
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-05 09:44:25 +0100

    minor fix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-  $('nav ul>li').click(function(){
+  $('nav ul > li').click(function(){
     var el = $(this).children('.dropdown');
     $('nav ul li').each(function(){
       $(this).children('.dropdown').slideUp();
     })
     el.is(':visible') ? el.slideUp() : el.slideDown();
 });
-
 
   $('.dropdown').mouseleave(function(){
     $('nav ul li').each(function(){
