b21d064f1c7d75aa6fd150a96a5e5f25d8fd3205
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-05 09:55:08 +0100

    last commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-  $('nav ul > li').click(function(){
-    var el = $(this).children('.dropdown');
-    $('nav ul li').each(function(){
-      $(this).children('.dropdown').slideUp();
-    })
-    el.is(':visible') ? el.slideUp() : el.slideDown();
+$('nav ul  li').click(function(){
+  var el = $(this).children('.dropdown');
+  $('nav ul li').each(function(){
+    $(this).children('.dropdown').slideUp();
+  })
+  el.is(':visible') ? el.slideUp() : el.slideDown();
 });
 
-  $('.dropdown').mouseleave(function(){
-    $('nav ul li').each(function(){
-      $(this).children('.dropdown').slideUp();
-    })
-  })
+
+$('.dropdown').mouseleave(function(){
+  $('nav ul li').children('.dropdown').slideUp();
+})
